aa9580836ea5a3e1e729b3522c682a3f6c9ecff5
Michael Joyner <joyner.michael@gmail.com>
2020-12-31 00:07:40 +0800

    wip

--- config/payfast.php
+++ config/payfast.php
@@ -4,6 +4,8 @@ return [
     'merchant_id' => env('PAYFAST_ID'),
     'merchant_key' => env('PAYFAST_KEY'),
     'passphrase' => env('PAYFAST_PASSPHRASE'),
+    'sandbox' => env('PAYFAST_SANDBOX'),
+
 
     'return_url' => env('APP_URL') . "/payfast/return",
     'cancel_url' => env('APP_URL') . "/payfast/cancel",
