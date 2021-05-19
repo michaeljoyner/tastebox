d567aea1b87a27881f48815365fdb61370606c68
Michael Joyner <joyner.michael@gmail.com>
2021-05-19 17:54:57 +0800

    wip

--- resources/views/front/blog/index.blade.php
+++ resources/views/front/blog/index.blade.php
@@ -1,7 +1,7 @@
 <x-public-page title="The TasteBox Blog" description="Follow the latest news, opinions, recipes and ramblings from the TasteBox team">
     <div class="py-20 max-w-3xl mx-auto px-6">
         <h1 class="type-h1 text-center">The TasteBox Blog</h1>
-        <p class="mt-6 max-w-xl mx-auto text-center">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Atque aut deleniti, deserunt eos placeat quidem suscipit! Accusamus asperiores, aspernatur, eligendi enim fugit laborum maiores obcaecati perspiciatis quia quibusdam sint totam.</p>
+        <p class="mt-6 max-w-xl mx-auto text-center">Whether it be quick and easy recipes, great cooking and kitchen tips, or updates on what TasteBox is up to and planning for the future, this is where you will find it.</p>
     </div>
 
     <div class="px-6">
