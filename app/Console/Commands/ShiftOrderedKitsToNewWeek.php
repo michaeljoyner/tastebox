dd372e96d21f804fa59a6d24db46056b5559631f
Michael Joyner <joyner.michael@gmail.com>
2021-07-14 09:01:23 +0800

    menu order shift

---
+++ app/Console/Commands/ShiftOrderedKitsToNewWeek.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Orders\Menu;
+use Illuminate\Console\Command;
+use Illuminate\Support\Str;
+
+class ShiftOrderedKitsToNewWeek extends Command
+{
+
+    protected $signature = 'orders:shift-menu {ids}';
+
+
+    protected $description = 'Shift orders from given menus to next week';
+
+
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+
+    public function handle()
+    {
+        $kits = collect(explode(',', $this->argument('ids')))
+            ->map(fn ($id) => Menu::find($id))
+            ->filter(fn ($m) => !!$m)
+            ->flatMap(fn ($menu) => $menu->orderedKits)
+            ->each(function ($kit) {
+                $next_menu = Menu::find($kit->menu_id + 1);
+                $kit->menu_id = $next_menu->id;
+                $kit->delivery_date = $next_menu->delivery_from;
+                $kit->menu_week_number = $next_menu->weekOfYear();
+                $kit->save();
+            });
+
+
+//        $menus->each(function(Menu $menu) {
+//           $menu
+//               ->orderedKits
+//               ->each(function($kit) use ($menu) {
+//                   $next_menu = Menu::find($menu->id + 1);
+//                   $kit->menu_id = $next_menu->id;
+//                   $kit->delivery_date = $next_menu->delivery_from;
+//                   $kit->menu_week_number = $next_menu->weekOfYear();
+//                   $kit->save();
+//                   $this->info("moved kit {$kit->id} to menu {$next_menu->weekOfYear()}");
+//               });
+//        });
+        return 0;
+    }
+}
