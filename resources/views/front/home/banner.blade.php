b083d8b3b5bb26cf8cec095d3095e9e41c02f666
Michael Joyner <joyner.michael@gmail.com>
2023-02-16 08:25:08 +0800

    wip

--- resources/views/front/home/banner.blade.php
+++ resources/views/front/home/banner.blade.php
@@ -1,12 +1,12 @@
 
-<div class="min-h-screen relative bg-tb-green bg-opacity-80 md:bg-opacity-100 overflow-hidden bg-mobile-banner md:bg-none">
-    <img src="/images/home/banner_chopping.jpg" class="hidden md:block absolute top-0 right-[70vw] h-screen"
+<div class="h-[660px] relative bg-tb-green bg-opacity-80 md:bg-opacity-100 overflow-hidden bg-mobile-banner md:bg-none">
+    <img src="/images/home/banner_chopping.jpg" class="hidden md:block absolute top-0 right-[70vw] min-[1550px]:right-[78vw] h-[660px] min-[2000px]:hidden"
          alt="">
 
-    <img src="/images/home/banner_noodles.jpg" class="hidden md:block absolute top-0 left-[70vw] h-screen"
+    <img src="/images/home/banner_noodles.jpg" class="hidden md:block absolute top-0 left-[70vw] min-[1550px]:left-[78vw] h-[660px] min-[2000px]:hidden"
          alt="">
 
-    <div class="relative flex flex-col w-full md:w-1/3 min-w-[350px] min-h-screen bg-tb-green bg-opacity-80 md:bg-opacity-100 mx-auto items-center pt-24">
+    <div class="relative flex flex-col w-full md:w-1/3 min-w-[350px] min-h-screen bg-tb-green bg-opacity-80 md:bg-opacity-100 mx-auto items-center pt-20">
         @include('svg.logos.logo_new', ['classes' => 'text-white opacity-50 h-20'])
         <h1 class="font-serif font-bold text-7xl text-white mt-8">TasteBox</h1>
         <p class="text-white text-xl font-serif text-center my-6">Healthy, ready-to-cook meal kits, delivered to your door, for only R95 per person.</p>
@@ -24,18 +24,28 @@
                 @include('svg.social.instagram', ['classes' => 'h-5 ml-2'])
             </a>
         </div>
+
+        <div class="mt-16">
+            <img src="/images/home/first_meal.png" class="max-w-[80vw] w-80 -rotate-3"
+                 alt="">
+            <div>
+                <a href="" class="text-white opacity-75 flex flex-col items-center -space-y-2 mt-4">
+                    <span>learn more</span>
+                    <span class="-mt-4">&downarrow;</span>
+                </a>
+            </div>
+        </div>
     </div>
 </div>
 
 <div class="py-20 px-8">
-    <div class="max-w-xl">
-        <p class="font-serif text-5xl font-bold">Your first meal's on us!</p>
-        <p class="my-6 text-slate-800 text-xl">Sign up now and get a 20% discount you can use on your first order. TasteBox members also get...</p>
-        <ul>
-            <li>Online access to your recipes and more.</li>
-            <li>Easier check-out.</li>
-            <li>Exclusive discounts.</li>
-            <li>Loyalty bonuses.</li>
-        </ul>
+    <div class="max-w-xl mx-auto">
+        <div class="flex flex-col items-center">
+            <p class="type-h1 text-center">Your first meal's on us!</p>
+            <p class="my-6 text-slate-800 text-xl text-center">Sign up now and get a <strong>20% discount</strong> you can use on your first order. TasteBox members also getonline access to their recipes, free weekly recipes, exclusive discounts and more!</p>
+            <div class="mt-12">
+                <a href="/register" class="green-btn">Become a member</a>
+            </div>
+        </div>
     </div>
 </div>
