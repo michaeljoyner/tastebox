a21fb0cf0078cb49fc4103601e7ae52da16ec4fd
Michael Joyner <joyner.michael@gmail.com>
2023-02-18 17:48:14 +0800

    wip

--- resources/views/front/home/banner.blade.php
+++ resources/views/front/home/banner.blade.php
@@ -9,7 +9,7 @@
     <div class="relative flex flex-col w-full md:w-1/3 min-w-[350px] min-h-screen bg-tb-green bg-opacity-80 md:bg-opacity-100 mx-auto items-center pt-20">
         @include('svg.logos.logo_new', ['classes' => 'text-white opacity-50 h-20'])
         <h1 class="font-serif font-bold text-7xl text-white mt-8">TasteBox</h1>
-        <p class="text-white text-xl font-serif text-center my-6">Healthy, ready-to-cook meal kits, delivered to your door, for only R95 per person.</p>
+        <p class="text-white text-xl font-serif text-center my-6 px-6">Healthy, ready-to-cook meal kits, delivered to your door, for only R95 per person.</p>
 
         <div class="flex items-center">
             <a href="/build-a-box" class="px-6 py-3 bg-tb-red hover:bg-tb-red-light text-white font-sans text-xs uppercase font-bold rounded-lg shadow-md">Build Your Box</a>
