3b9ea503d1fceaba9725b84c5c50d16aca204381
Michael Joyner <joyner.michael@gmail.com>
2023-03-04 12:13:33 +0800

    easter banner

--- resources/views/front/home/easter-banner.blade.php
+++ resources/views/front/home/easter-banner.blade.php
@@ -1,3 +1,3 @@
-<div class="bg-green-600 text-white px-8 py-4 {{ $classes ?? '' }}">
-    <p class="text-center font-semibold">Please note that there will be no menu available for delivery the week after Easter.</p>
+<div class="bg-slate-800 text-white px-8 py-4 {{ $classes ?? '' }}">
+    <p class="text-center font-semibold">We will close for Easter weekend, so there will be no menu available for delivery on April 11th.</p>
 </div>
