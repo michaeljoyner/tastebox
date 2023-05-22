3557ca655cff4516da26c60ae2e4d3144719d01d
Michael Joyner <joyner.michael@gmail.com>
2023-05-22 11:56:54 +0800

    comrades banner

--- resources/views/front/home/easter-banner.blade.php
+++ resources/views/front/home/easter-banner.blade.php
@@ -1,3 +1,3 @@
 <div class="bg-slate-800 text-white px-8 py-4 {{ $classes ?? '' }}">
-    <p class="text-center font-semibold">We will close for Easter weekend, so there will be no menu available for delivery on April 11th.</p>
+    <p class="text-center font-semibold">We will close for Comrades Marathon weekend, so there will be no menu available for delivery on June 13th.</p>
 </div>
