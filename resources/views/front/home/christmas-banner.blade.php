2b4034eed38fd741908f991e90c0a73f61e93568
Michael Joyner <joyner.michael@gmail.com>
2023-11-16 20:15:49 +0800

    x-mas banner

--- resources/views/front/home/christmas-banner.blade.php
+++ resources/views/front/home/christmas-banner.blade.php
@@ -1,10 +1,10 @@
-<div class="bg-green-600 text-white px-8 py-4">
+<div class="bg-emerald-800 text-white px-8 py-4">
     <div class="flex space-x-4 items-center justify-center">
         @include('svg.xmas', ['classes' => 'w-6 h-6 text-green-700'])
         <p class="type-h3 text-center">Happy Holidays!</p>
         @include('svg.xmas', ['classes' => 'w-6 h-6 text-green-700'])
     </div>
     <p class="type-b1 text-center">Deliveries closed from
-        <span class="type-b2">Dec 14th</span> - <span class="type-b2">Jan 10th</span>. Our <a href="/build-a-box" class="type-b2 hover:underline">2023 menus</a>  are open now.</p>
+        <span class="type-b2">Dec 13th</span> - <span class="type-b2">Jan 9th</span>. Our <a href="/build-a-box" class="type-b2 hover:underline">2024 menus</a>  are open now.</p>
 
 </div>
