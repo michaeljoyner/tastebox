e9d420eae9aad5e0ca0533ad61b2d100e27c9d11
Michael Joyner <joyner.michael@gmail.com>
2025-01-05 20:07:36 +0200

    change mention of dates

--- resources/views/front/home/delivery.blade.php
+++ resources/views/front/home/delivery.blade.php
@@ -1,7 +1,7 @@
 <div class="py-20 px-6">
     <p class="type-h1 mb-4 text-center">How We Deliver</p>
-    <p class="type-h2 mb-6 text-center">Every Tuesday, to your door.</p>
-    <p class="max-w-2xl mx-auto text-center my-6">We currently deliver within Pietermaritzburg and the surrounding areas, but please refer to our FAQs for the complete list of delivery locations. Orders close every week on Wednesday at midnight, with delivery the following Tuesday. Once you have placed your order online, somebody from the TasteBox team will contact you to arrange the most convenient time and place to receive your delivery. </p>
+    <p class="type-h2 mb-6 text-center">Every Monday, to your door.</p>
+    <p class="max-w-2xl mx-auto text-center my-6">We currently deliver within Pietermaritzburg and the surrounding areas, but please refer to our FAQs for the complete list of delivery locations. Orders close every week on Friday at midday, with delivery the following Monday. Once you have placed your order online, somebody from the TasteBox team will contact you to arrange the most convenient time and place to receive your delivery. </p>
     <div class="flex justify-center mt-20">
         <a href="/build-a-box"
            class="green-btn">See upcoming menus</a>
