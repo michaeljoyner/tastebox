0ab15a493c8b036259420edcfc4241aa826b5267
Michael Joyner <joyner.michael@gmail.com>
2025-03-19 07:37:59 +0800

    easter menu banner

--- resources/views/front/home/easter-banner.blade.php
+++ resources/views/front/home/easter-banner.blade.php
@@ -1,3 +1,3 @@
 <div class="bg-slate-800 text-white px-8 py-4 {{ $classes ?? '' }}">
-    <p class="text-center font-semibold">We will close for the Easter Weekend, so there will be no menu available for delivery on April 2nd.</p>
+    <p class="text-center font-semibold">We will close for the Easter Weekend, so there will be no menu available for delivery on April 21st.</p>
 </div>
--- resources/views/front/kit-builder/page.blade.php
+++ resources/views/front/kit-builder/page.blade.php
@@ -1,4 +1,5 @@
 <x-public-page title="Build Your Box | Meal Kits Delivered to Your Door" :css="mix('css/front.css')"  :no-robots="true">
+@include('front.home.easter-banner')
     <div class="bg-green-600 h-2 w-full"></div>
 
     <kit-manager :registered="{{ auth()->user() ? 'true' : 'false' }}" initial-kit="{{ $kit }}" initial-view="{{ $showView }}" :menus='@json($menus)' :initial-basket='@json($basket)'></kit-manager>
