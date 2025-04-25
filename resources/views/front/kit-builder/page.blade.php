db49b34dbe1664442c7c80faae7d6b51993cbbe9
Michael Joyner <joyner.michael@gmail.com>
2025-04-25 20:33:13 +0800

    remove easter banner

--- resources/views/front/kit-builder/page.blade.php
+++ resources/views/front/kit-builder/page.blade.php
@@ -1,5 +1,4 @@
-<x-public-page title="Build Your Box | Meal Kits Delivered to Your Door" :css="mix('css/front.css')"  :no-robots="true">
-@include('front.home.easter-banner')
+<x-public-page title="Build Your Box | Meal Kits Delivered to Your Door" :css="mix('css/front.css')" :no-robots="true">
     <div class="bg-green-600 h-2 w-full"></div>
 
     <kit-manager :registered="{{ auth()->user() ? 'true' : 'false' }}" initial-kit="{{ $kit }}" initial-view="{{ $showView }}" :menus='@json($menus)' :initial-basket='@json($basket)'></kit-manager>
