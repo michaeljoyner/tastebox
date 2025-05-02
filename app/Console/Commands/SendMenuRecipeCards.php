549bb177e9bd586f931572904dfa678ef093c04f
Michael Joyner <joyner.michael@gmail.com>
2025-05-02 17:02:04 +0800

    send recipe cards to mom

--- app/Console/Commands/SendMenuRecipeCards.php
+++ app/Console/Commands/SendMenuRecipeCards.php
@@ -3,40 +3,38 @@
 namespace App\Console\Commands;
 
 use App\Mail\SendRecipeCards;
-use App\Meals\Meal;
 use App\Meals\RecipeCard;
 use App\Orders\Menu;
 use Illuminate\Console\Command;
 use Illuminate\Support\Facades\Mail;
-use Illuminate\Support\Str;
 
 class SendMenuRecipeCards extends Command
 {
-
     protected $signature = 'menus:weekly-recipes';
 
-
     protected $description = 'Create and send the current menu\'s recipe cards';
-
 
     public function __construct()
     {
         parent::__construct();
     }
 
-
     public function handle()
     {
         $menu = Menu::nextToPrep();
 
-        if(!$menu->id) {
+        if (! $menu->id) {
             $this->warn('menu is not prepared');
+
             return 0;
         }
 
         $archive = RecipeCard::archiveForMenu($menu);
 
         Mail::to('stephjoyner18@gmail.com')
+            ->queue(new SendRecipeCards(RecipeCard::disk()->path($archive)));
+
+        Mail::to('alexandra.joyner@gmail.com')
             ->queue(new SendRecipeCards(RecipeCard::disk()->path($archive)));
 
         return 0;
