5c599e3284d048c62816a79f290260962dd7c735
Michael Joyner <joyner.michael@gmail.com>
2026-01-16 19:36:30 +0800

    rm xmas banner

--- resources/views/front/home/page.blade.php
+++ resources/views/front/home/page.blade.php
@@ -1,6 +1,5 @@
 <x-public-page title="TasteBox | Meal Kits Delivered to Your Door"
     description="The fun and affordable way to cook healthy meals at home." :has-slideshow="true">
-    @include('front.home.christmas-banner')
     @include('front.home.banner')
     @include('front.home.how-it-works')
     @include('front.home.current-menu')
