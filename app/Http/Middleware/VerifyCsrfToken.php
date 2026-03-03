3c0cb1ce2d4581641eb0a19be2e1ab9899ea5987
Michael Joyner <joyner.michael@gmail.com>
2026-03-03 14:50:36 +0200

    disable csrf for route

--- app/Http/Middleware/VerifyCsrfToken.php
+++ app/Http/Middleware/VerifyCsrfToken.php
@@ -12,6 +12,7 @@ class VerifyCsrfToken extends Middleware
      * @var array
      */
     protected $except = [
-        'payfast/notify/*'
+        'payfast/notify/*',
+        'forge-deploys',
     ];
 }
